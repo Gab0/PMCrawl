fc44bfbc25012da78f795a71cddec2ef44216cb0
Gabriel Araujo <gabriel_scf@hotmail.com>
2019-05-17 15:54:14 -0300

    article id list updates.

--- pmccrawl/Options.py
+++ pmccrawl/Options.py
@@ -17,17 +17,30 @@ parser.add_option('--refreshList', dest='RefresList', action='store_true', defau
 parser.add_option('-r', '--recent', dest='Recent', default=None, type="int")
 parser.add_option('-R', '--relevance', dest='Relevance', default=None, type="int")
 parser.add_option('-F', '--filter', dest='Filter', default='')
-parser.add_option('-a', '--async', dest='ASYNC', action='store_true',
-                  default=False, help='Run in async mode.')
+#parser.add_option('-a', '--async', dest='ASYNC', action='store_true',
+#                  default=False, help='Run in async mode.')
 
-parser.add_option('-l', dest='ShowList', action='store_true', default=False, help='show list of results as csv PMIDs')
+# Output List Options;
+parser.add_option('-l', dest='ShowList',
+                  action='store_true',
+                  default=False,
+                  help='show list of results as csv PMIDs')
 
-# build lists options;
-parser.add_option('-d', dest='makeDoiList', action='store_true', default=False, help='Write a doi list of search results.')
+parser.add_option(
+    '--la',
+    dest='ListAttributes',
+    default="p",
+    help='Order of article attributes to be written in list.\n' +
+    'This is a string of chars, if the first is missing' +
+    ' it writes the second and so on. Set: <pdi> -> PMC, DOI, ID'
+)
 
-parser.add_option('-P',
-                  dest='makePMCIDList',
-                  help='Write a pmcid list of search results to specified file.')
+parser.add_option(
+    '--lp',
+    dest="ListPath",
+    default="articlelist.txt",
+    help="Selects the path to save the article list."
+)
 
 parser.add_option('-A', dest='saveAbstractBatch', action='store_true',
                   default=False, help='Save abstracts of all results on text file.')
